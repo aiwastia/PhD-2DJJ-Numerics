45ec71242e1dc2d5295a7d440b9219cc5f4b655b
aiwastia <aiwastia@pks.mpg.de>
2019-09-18 18:30:52 +0200

    Import files

---
+++ NumSpecFortran/batches/src/JJ_gap.f90
@@ -0,0 +1,188 @@
+program PD
+
+! calculate spectrum using Friedel sum rule (see Eq. (4) in PRL 107, 196804 (2011))
+
+use io
+use selectedsubr
+use scans
+
+implicit none
+
+real*8              :: Deltar,Deltai,Deltar1,Deltai1
+real*8              :: gammatot, bmagx, kx, mubuff, mubuff1, kxmax, kxmin, phi, normres, eps,&
+			            Ekx, Ekx1, Ekx2, kFmax, dkx, gap
+real                ::tarray(2), result, tottime
+integer             :: n, i, jj, Opnbr, Opnbr1,nkxmax
+
+real*8,dimension(5)		:: vardata
+character(len=3) 	:: tempstr
+
+character(len=4),dimension(6) :: vartitle
+integer :: Ai
+real*8,allocatable,dimension(:) :: Aphiarray,ABarray,AmuSCarray,Amuarray,ASCmarray
+
+integer :: g,gg,ggg,gggg,ggggg, g2,lgth
+real*8 :: Aloopparam(6)
+character(len=100) :: Alecture
+integer :: Apointer
+
+
+!!!!!!! start
+tottime=0
+
+
+call get_infile_newparameters
+
+open(unit=30,file=dataname,form='formatted',status='new')
+
+vartitle=(/ '#phi','B   ','muSC','mu  ','SCm ','gap ' /)
+write(30,"(6(a,a,a))") (vartitle(i),achar(9),achar(9),i=1,6)
+
+call makelist(trim(Aphilist),Aphiarray,'phi')
+Anstepphi=size(Aphiarray)
+call makelist(trim(ABlist),ABarray,'B')
+AnstepB=size(ABarray)
+call makelist(trim(AmuSClist),AmuSCarray,'muSC')
+AnstepmuSC=size(AmuSCarray)
+call makelist(trim(Amulist),Amuarray,'mu')
+Anstepmu=size(Amuarray)
+call makelist(trim(ASCmlist),ASCmarray,'SCmass')
+AnstepSCm=size(ASCmarray)
+
+
+!! LOOPS TO COMPUTE THE GAP USING THE SCATTERING CODE
+
+do g=1,Anstepphi
+	Aloopparam(1)=Aphiarray(g)
+do gg=1,AnstepB
+	Aloopparam(2)=ABarray(gg)
+do ggg=1,AnstepmuSC
+	Aloopparam(3)=AmuSCarray(ggg)
+do gggg=1,Anstepmu
+	Aloopparam(4)=Amuarray(gggg)
+do ggggg=1,AnstepSCm
+	Aloopparam(5)=ASCmarray(ggggg)
+	
+	!! MODIFYING THE INPUT FILE
+	Apointer=0
+	open(unit=22,file='infile_JJ_bands_1',form='formatted',status='old')
+	do while (Apointer.eq.0)
+		read(22,'(a4)') Alecture
+		if (Alecture.eq.'mass') then
+			Apointer=1
+			write(22,*) 'Deltaphase2',achar(9),'=',Aloopparam(1)
+			write(22,*) 'bmag',achar(9),achar(9),'=',Aloopparam(2)
+			write(22,*) 'mu0',achar(9),achar(9),'=',Aloopparam(3)
+			write(22,*) 'mu01',achar(9),achar(9),'=',Aloopparam(4)
+			write(22,*) 'SCmass',achar(9),achar(9),'=',Aloopparam(5)
+			write(22,*) '/'
+		end if
+	end do
+	close(22)
+
+!!!!!!!!!!!!!!! COMPUTE THE GAP WITH THE NEW INPUT => will be stored in Aloopparam(5) for printing by line
+
+call get_infile_parameter
+
+!!!!!!!!!! PARAMETERS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+Deltaphase=0d0 !common shift of the phases
+Deltaphase1=0d0 ! in junction
+btheta=0d0
+btheta1=0d0
+bmag1=bmag ! same magnetic field in lead and junction, but bmag=>bmag*0 later
+alpha1=alpha !same SOC in the lead and in the junction
+
+gammatot = 0 !disorder amplitude
+varphase='scLR' !specify the system (SC on left and right)
+
+! SC phase difference in units of 2*pi
+phi = Deltaphase2
+
+Deltar = Deltamag * cos(Deltaphase*pi)
+Deltai = Deltamag * sin(Deltaphase*pi)
+Deltar1 = Deltamag1 * cos(Deltaphase1*pi)
+Deltai1 = Deltamag1 * sin(Deltaphase1*pi)
+
+kFmax=mass*alpha+sqrt(2d0*mass*(mu0+bmag)+mass**2*alpha**2) !in junction
+kxmax=1.1*kFmax
+kxmin=0. !1.0*kFmax-2*kFmax/(kFmax*Lmax1)**2
+dkx=kFmax/(kFmax*Lmax1)**2 / dkfrac
+nkxmax=int((kxmax-kxmin)/dkx)
+
+!!!!!!!!!!! SPACE DISCRETIZATION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ !two leads
+ if (Lmax.ge.dL) then
+     Opnbr=min(30,int(log(Lmax/dL)/log(2d0))+1)
+     Nseg = 2**Opnbr
+     dL0 = Lmax / dble(Nseg)
+ else
+     dL0=dL
+     Nseg=0
+ endif 
+
+ !junction
+ if (Lmax1.gt.dL) then
+     Opnbr1=min(30,int(log(Lmax1/dL)/log(2d0))+1)
+     Nseg1 = 2**Opnbr1
+     dL1 = Lmax1 / dble(Nseg1)
+ else
+     dL1=dL
+     Nseg1=0
+ endif
+
+
+!!!!!!!!!!!!!!!!!!! VERTICAL SECANTE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!call vertical_secante(nkxmax,kxmin,kxmax,dL0,dL1,&
+!			gammatot,mu0,mu01,Deltamag,Deltar,Deltar1,Deltai,Deltai1,alpha,alpha1,&
+!			bmag,bmag1,btheta,btheta1,potshift,SCmass,mass,&
+!			Opnbr,Opnbr1,pot,Lj,phi,varphase,gap,&
+!			epsmin,epsmax,secant)
+
+
+!!!!!!!!!!!!!!!!!!! VERTICAL DYNAMICAL SECANTE !!!!!!!!!!!!!!!!!!!!!!
+call dynamical_secante(nkxmax,kxmin,kxmax,dL0,dL1,&
+            gammatot,mu0,mu01,Deltamag,Deltar,Deltar1,Deltai,Deltai1,alpha,alpha1,&
+            bmag,bmag1,btheta,btheta1,potshift,SCmass,mass,&
+            Opnbr,Opnbr1,pot,Lj,phi,varphase,gap,&
+            epsmin,epsmax,secant,5)  !!can ask for the maximal depth, will stop at the last relevant
+
+
+!sum up the running time of each loop
+call dtime(tarray, result)
+tottime=tottime+result
+
+
+	Aloopparam(6)=gap
+	write(30,'(F8.6,a,F8.4,a,F8.3,a,F8.4,a,F8.4,a,F15.12,a)') (Aloopparam(g2),achar(9),g2=1,6) !fill in each column of data to plot
+    flush(30)
+
+end do !SCmass
+end do !mu
+end do !muSC
+end do !B
+end do !phi
+
+close(30)
+open(unit=31,file='infile_JJ_2',status='old') !overwrite the number of steps in each loop (to be used in plots)
+do i=1,9 !newlines are counted in
+	read(31,*)
+enddo
+write(tempstr,'(I3)') Anstepphi
+write(31,'(a,a,a,a)') 'Anstepphi',achar(9),'=',tempstr
+write(tempstr,'(I3)') AnstepB
+write(31,'(a,a,a,a,a)') 'AnstepB',achar(9),achar(9),'=',tempstr
+write(tempstr,'(I3)') AnstepmuSC
+write(31,'(a,a,a,a)') 'AnstepmuSC',achar(9),'=',tempstr
+write(tempstr,'(I3)') Anstepmu
+write(31,'(a,a,a,a)') 'Anstepmu',achar(9),'=',tempstr
+write(tempstr,'(I3)') AnstepSCm
+write(31,'(a,a,a,a)') 'AnstepSCm',achar(9),'=',tempstr
+write(31,'(a)') '/'
+close(31)
+
+call dtime(tarray, result)
+tottime=tottime+result
+print*,'#total number of loops for Phi, B, muSC, mu, SCmass!',Anstepphi,AnstepB,AnstepmuSC,Anstepmu,AnstepSCm
+write(*,*) "#total time(min) = ", tottime/60.0
+
+end program
---
+++ NumSpecFortran/batches/src/scans.f90
@@ -0,0 +1,261 @@
+module scans
+use selectedsubr
+
+implicit none
+
+
+contains
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!! ENERGY SCANNING kx(E) at fixed spacing !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine computeKxe(Ndata,epsmin,epsmax,& !!meaning nkxmax kxmin kxmax
+			dL0,dL1,&
+			gammatot,mubuff,mubuff1,Deltar,Deltar1,Deltai,Deltai1,alpha,alpha1,&
+			bmag,bmag1,btheta,btheta1,potshift,SCmass,mass,&
+			Opnbr,Opnbr1,pot,Lj,phi,varphase,kx,kroot,newlevelLIST,nl,gaptest,&
+            detR,active,insite) !!kx meaning energy level En
+
+integer, intent(in)	:: Ndata,Opnbr,Opnbr1,active
+real*8,intent(in) 	:: epsmin,epsmax,dL0,dL1,&
+			Deltar,Deltai,Deltar1,Deltai1,alpha,alpha1,mubuff,mubuff1,SCmass,mass,&
+			gammatot,potshift,pot,Lj,bmag,bmag1,btheta,btheta1,phi,kx
+character(len=200),intent(in) :: varphase
+real*8,intent(out)	:: newlevelLIST(20)
+integer, intent(out) :: insite,nl
+integer,intent(inout) :: gaptest
+complex*16,intent(inout) :: detR
+
+integer :: keps,n,i,kroot
+real*8 :: eps,newlevel,bmagx,mu0,mu01
+complex*16 :: dS(8,8),S1(8,8),S2(8,8),S3(8,8),Sbar(8,8),unitmat(8,8),Sb(8,8)
+complex*16 :: detRold
+logical :: intest
+
+!! comment : (*) means kx <-> eps
+
+nl=0
+kroot=0
+newlevelLIST=epsmax
+intest=.false.
+insite=0
+
+do keps=0,Ndata !energy loop
+
+    eps=epsmin + (epsmax - epsmin)/dble(Ndata) * dble(keps) !scan in energy
+	bmagx =-alpha*eps !(*)
+	mu0 = mubuff-eps**2/2d0/SCmass !(*)
+	mu01 = mubuff1-eps**2/2d0/mass !(*)
+	if (mu0+potshift<0) write(*,*) 'mu0+potshift negative: ', mu0, '+',potshift,'<0'
+	if (mu01+potshift<0) write(*,*) 'mu0+potshift negative: ', mu01, '+',potshift,'<0'
+   
+   ! initialize matrix as unit matrix
+   unitmat=dcmplx(0,0)
+   forall (i=1:8) unitmat(i,i)=dcmplx(1,0)
+   S1=unitmat
+   S2=unitmat
+   S3=unitmat
+   Sbar=0
+   do i=1,4
+         Sbar(i,i+4) = dcmplx(-1.d0,0.d0)
+         Sbar(i+4,i) = dcmplx(-1.d0,0.d0)
+   enddo
+
+
+   !lead 1
+   call deltaSwire_1ch(dS,1,kx,dL0,gammatot,mu0,Deltar,Deltai,alpha,0d0*bmag,bmagx,btheta,potshift,SCmass) !(*)
+   call concat(dS,S1,S1,4)
+   do n=1,Opnbr
+      call concat(S1,S1,S1,4)
+   enddo
+
+   !junction
+   call deltaSwire_1ch(dS,1,kx,dL1,gammatot,mu01,Deltar1,Deltai1,alpha1,bmag1,bmagx,btheta1,potshift,mass) !(*)
+   call concat(dS,S2,S2,4)
+   do n=1,Opnbr1
+      call concat(S2,S2,S2,4)
+   enddo
+
+   !lead 2
+   S3 = S1
+
+   !add potential barrier at both interfaces to include some normal reflection
+   call Sbarrier(Sb,mu0, kx, pot, Lj, potshift, mass, SCmass)
+   call concat(Sb,S2,S2,4)
+   call concat(S2,Sb,S2,4)
+
+   !compute determinant
+   detRold = detR
+   detR = detringconcat(S1,S2,S3,Sbar,phi/2.,varphase)
+
+    if ((active.eq.1).and.(intest)) insite=insite+1
+
+   ! detR = det(1-S), this changes sign when there is a state
+   ! the change in sign for both real and imaginary parts of the determinant is a sufficient condition for non-degenerate states
+   if ((keps.gt.0).and.(dreal(detR)*dreal(detRold).lt.0d0).and.(dimag(detR)*dimag(detRold).lt.0d0) ) then 
+     kroot=1
+     newlevel=eps - (epsmax - epsmin)/dble(Ndata)/2d0 !backward extrapolation of a half step for better accuracy
+     nl=nl+1
+     newlevelLIST(nl)=newlevel
+     intest=.not.intest
+   endif
+enddo
+
+if(kroot.eq.1) gaptest=gaptest+1
+
+end subroutine computeKxe
+
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!! VERTICAL secante along E !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+subroutine vertical_secante(Ndata,epsmin,epsmax,& !! meaning nkxmax,kxmin,kxmax
+			dL0,dL1,&
+			gammatot,mu0,mu01,Deltamag,Deltar,Deltar1,Deltai,Deltai1,alpha,alpha1,&
+			bmag,bmag1,btheta,btheta1,potshift,SCmass,mass,&
+			Opnbr,Opnbr1,pot,Lj,phi,varphase,gap,&
+			kxminbuff,kxmaxbuff,secant) !! meaning epsmin,epsmax,Ndata
+
+integer, intent(in)	:: Ndata,Opnbr,Opnbr1,secant
+real*8,intent(in) 	:: epsmin,epsmax,dL0,dL1,&
+			Deltamag,Deltar,Deltai,Deltar1,Deltai1,alpha,alpha1,mu0,mu01,SCmass,mass,&
+			gammatot,potshift,pot,Lj,bmag,bmag1,btheta,btheta1,phi,&
+			kxminbuff,kxmaxbuff
+character(len=200),intent(in) :: varphase
+real*8,intent(out) :: gap
+
+integer :: jj,nkx,gaptest,sec,insite,kroot,nl
+real*8 :: kx,kxmin,kxmax,deps
+real*8 :: newlevelLIST(20)
+complex*16 :: detR
+
+
+kxmin=kxminbuff
+kxmax=kxmaxbuff
+gaptest=0
+
+do sec=1,secant
+	kx = (kxmax+kxmin)/2d0
+	call computeKxe(Ndata,epsmin,epsmax,dL0,dL1,&
+			gammatot,mu0,mu01,Deltar,Deltar1,Deltai,Deltai1,alpha,alpha1,&
+			bmag,bmag1,btheta,btheta1,potshift,SCmass,mass,&
+			Opnbr,Opnbr1,pot,Lj,phi,varphase,kx,kroot,newlevelLIST,nl,gaptest,&
+            detR,0,insite)
+	
+    if(gaptest.eq.1) then
+        kxmax=kx
+        gaptest=0
+    else
+        kxmin=kx
+        gaptest=0
+    endif
+
+enddo
+
+gap=(kxmax+kxmin)/2d0
+
+end subroutine vertical_secante
+
+
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!! VERTICAL secante & DYNAMICAL kx !!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+subroutine dynamical_secante(Ndatabuff,epsminbuff,epsmaxbuff,& !! meaning nkxmax,kxmin,kxmax
+            dL0,dL1,&
+            gammatot,mu0,mu01,Deltamag,Deltar,Deltar1,Deltai,Deltai1,alpha,alpha1,&
+            bmag,bmag1,btheta,btheta1,potshift,SCmass,mass,&
+            Opnbr,Opnbr1,pot,Lj,phi,varphase,gap,&
+            kxminbuff,kxmaxbuff,secant,depth) !! meaning epsmin,epsmax,Ndata
+
+integer, intent(in)    :: Ndatabuff,Opnbr,Opnbr1,secant,depth
+real*8,intent(in)     :: epsminbuff,epsmaxbuff,dL0,dL1,&
+            Deltamag,Deltar,Deltai,Deltar1,Deltai1,alpha,alpha1,mu0,mu01,SCmass,mass,&
+            gammatot,potshift,pot,Lj,bmag,bmag1,btheta,btheta1,phi,&
+            kxminbuff,kxmaxbuff
+character(len=200),intent(in) :: varphase
+real*8,intent(out) :: gap
+
+integer :: jj,nkx,gaptest,sec,insite,Ndata,depthc,kroot,nl
+real*8 :: kx,kxmin,kxmax,deps,epsmin,epsmax
+real*8 :: newlevelLIST(20)
+complex*16 :: detR
+logical :: convtest
+
+
+kxmin=kxminbuff
+kxmax=kxmaxbuff
+epsmin=epsminbuff
+epsmax=epsmaxbuff
+Ndata=Ndatabuff
+depthc=depth
+
+gaptest=0
+!convtest=.false.
+
+do sec=1,secant
+    kx = (kxmax+kxmin)/2d0
+    call computeKxe(Ndata,epsmin,epsmax,dL0,dL1,&
+                gammatot,mu0,mu01,Deltar,Deltar1,Deltai,Deltai1,alpha,alpha1,&
+                bmag,bmag1,btheta,btheta1,potshift,SCmass,mass,&
+                Opnbr,Opnbr1,pot,Lj,phi,varphase,kx,kroot,newlevelLIST,nl,gaptest,&
+                detR,1,insite)
+    !print*,insite
+    !print*, epsmin,epsmax
+    !print*,kx
+    if(gaptest.eq.1) then !ie if kx cut the spectrum
+        if((insite.eq.1).and.(nl.eq.2)) then !ie if there is a single point of the grid at the bottom of the minimum
+            gap=(kxmax+kxmin)/2d0
+            depthc=depthc-1
+            if (depthc.lt.0) then
+                !convtest=.true.
+                exit
+            endif
+            deps=(epsmax-epsmin)/dble(Ndata)
+            epsmin=newlevelLIST(1)-deps/2.
+            epsmax=newlevelLIST(1)+deps
+            !print*,epsmin,epsmax
+            Ndata=100
+            kxmin=kxminbuff
+            kxmax=kx
+            gaptest=0
+        else  !!ie insite > 1 because insite=0 is excluded by gaptest=1
+            kxmax=kx
+            gap=(kxmax+kxmin)/2d0
+            gaptest=0
+        endif
+    else !ie if kx doesn't cut the spectrum
+        if(abs(kxmin-kxmax).le.1d-13) then !doesn't mean the precision on the gap
+! because it depends on the horizontal mesh grid
+! The precisions tend to the same value for large depth, but it's beyond machine precision on these numbers (here)
+            !print*,'check'
+            gap=kxmin
+            exit
+        endif
+        kxmin=kx
+        gaptest=0
+    endif
+enddo
+
+!if (convtest) then
+!    gap=(kxmax+kxmin)/2.
+!else
+!    gap=kxmaxbuff
+!    write(30,*) "Gap did not converge. secant is too low for this depth."
+!endif
+
+end subroutine dynamical_secante
+
+
+
+
+end module
+
+
+
