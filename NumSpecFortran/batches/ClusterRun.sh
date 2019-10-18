4d1048c7f9e49d2274226a30fc26e3133790be1f
aiwastia <aiwastia@pks.mpg.de>
2019-10-18 13:02:58 +0200

    more tolerance

--- NumSpecFortran/batches/ClusterRun.sh
+++ NumSpecFortran/batches/ClusterRun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 #SBATCH -D /home/aiwastia/Desktop/git/PhD-2DJJ-Numerics/NumSpecFortran/batches
 #SBATCH --partition=long
-#SBATCH --job-name=1massSOC
+#SBATCH --job-name=timer3
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=10G
 #SBATCH --time=300:00:00
 
 #SBATCH --output make.out
